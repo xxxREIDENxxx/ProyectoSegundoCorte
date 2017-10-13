0d408132092b46d2ab5588f8e0cde908d69fa995
xxxREIDENxxx <reiden-3@hotmail.com>
2017-10-13 11:10:09 -0500

    segundo commit

--- src/CajaRegistradora1/CajaRegistradora1.java
+++ src/CajaRegistradora1/CajaRegistradora1.java
@@ -4,7 +4,8 @@ public class CajaRegistradora1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		VentanaPrincipal a = new  VentanaPrincipal();
+		a.setVisible(true);
 	}
 
 }
